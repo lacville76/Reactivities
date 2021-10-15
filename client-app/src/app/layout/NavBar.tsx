cd3a7c0954a770ae87b7a652c6235f818d1c4eb9
Wojciech Bąk <lacville76@gmail.com>
2021-10-15 17:48:12 +0200

    End of section 10

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -8,6 +8,7 @@ export default function NavBar() {
               <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}></img>
           </Menu.Item>
           <Menu.Item as={NavLink} to='/activities' name="Activities"></Menu.Item>
+          <Menu.Item as={NavLink} to='/errors' name="Errors"></Menu.Item>
           <Menu.Item>
               <Button as={NavLink} to='/createActivity' positive content='Create Activity'></Button>
           </Menu.Item>

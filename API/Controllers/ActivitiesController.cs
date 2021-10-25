d6deaebb1b08ef2211e4c4037271a0f0873ac7bd
Wojciech Bąk <lacville76@gmail.com>
2021-10-25 19:33:05 +0200

    End of section 14

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities;
-using System.Threading;
 using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
-{   
-    [AllowAnonymous]
+{
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
@@ -29,7 +26,8 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
-        
+
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity) 
         {
@@ -37,10 +35,17 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id) 
         {
             return HandleResult(await Mediator.Send(new Delete.Command {Id = id}));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command{Id = id}));
         }
     }
 }
